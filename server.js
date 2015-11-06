2c078f4f8767f5ef597ce13ac49be8162407d93f
kofiachie <koajunior92@gmail.com>
2015-11-06 01:46:46 -0500

    server to serve static files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 app.use(express.static(__dirname + "/public"));
-app.listen(3000,function(){
-  console.log("Server listening on port 3000");
+app.listen(port,function(){
+  console.log("Server listening on port " + port);
 });
